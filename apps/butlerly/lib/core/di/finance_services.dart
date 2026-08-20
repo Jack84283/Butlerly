498bfad5f423aeaf27f27da5db8a93145ba7d92d
Jack <jack84283@gmail.com>
2026-08-20 14:56:03 -0700

    Restore finance service formatting for Step 2

--- apps/butlerly/lib/core/di/finance_services.dart
+++ apps/butlerly/lib/core/di/finance_services.dart
@@ -2,8 +2,88 @@ import 'package:butlerly_finance_application/butlerly_finance_application.dart';
 import 'package:butlerly_finance_domain/butlerly_finance_domain.dart';
 
 final class FinanceServices {
-  FinanceServices(TransactionRepository transactions, PaymentSourceRepository paymentSources, MerchantRepository merchants, CategoryRepository categories, TagRepository tags, EvidenceRepository evidence, UserPreferenceRepository preferences)
-      : listTransactions = ListTransactions(transactions), seedInitialMasterData = SeedInitialMasterData(merchants, categories, tags), createTransaction = CreateTransaction(transactions, const SystemApplicationClock()), createReceiptTransaction = CreateReceiptTransaction(transactions, const SystemApplicationClock()), importTransaction = ImportTransaction(transactions, const SystemApplicationClock()), getTransaction = GetTransaction(transactions), updateTransaction = UpdateTransaction(transactions, const SystemApplicationClock()), archiveTransaction = ArchiveTransaction(transactions, const SystemApplicationClock()), restoreTransaction = RestoreTransaction(transactions, const SystemApplicationClock()), deleteTransactionPermanently = DeleteTransactionPermanently(transactions), listPaymentSources = ListPaymentSources(paymentSources), savePaymentSource = SavePaymentSource(paymentSources), archivePaymentSource = ArchivePaymentSource(paymentSources), assignPaymentSource = AssignPaymentSource(transactions, paymentSources, const SystemApplicationClock()), listMerchants = ListMerchants(merchants), listCategories = ListCategories(categories), listTags = ListTags(tags), saveMerchant = SaveMerchant(merchants), saveCategory = SaveCategory(categories), saveTag = SaveTag(tags), assignMerchant = AssignMerchant(transactions, merchants, const SystemApplicationClock()), assignCategory = AssignCategory(transactions, categories, const SystemApplicationClock()), addTag = AddTag(transactions, tags, const SystemApplicationClock()), removeTag = RemoveTag(transactions, const SystemApplicationClock()), listReviewItems = ListReviewItems(transactions), resolveReviewIssue = ResolveReviewIssue(transactions, const SystemApplicationClock()), dismissReviewIssue = DismissReviewIssue(transactions, const SystemApplicationClock()), listEvidenceForTransaction = ListEvidenceForTransaction(evidence), loadUserPreference = LoadUserPreference(preferences), saveUserPreference = SaveUserPreference(preferences), storeAndAttachEvidence = StoreAndAttachEvidence(evidence), removeEvidence = RemoveEvidence(evidence), saveExtraction = SaveExtraction(evidence);
+  FinanceServices(
+    TransactionRepository transactions,
+    PaymentSourceRepository paymentSources,
+    MerchantRepository merchants,
+    CategoryRepository categories,
+    TagRepository tags,
+    EvidenceRepository evidence,
+    UserPreferenceRepository preferences,
+  ) : listTransactions = ListTransactions(transactions),
+      seedInitialMasterData = SeedInitialMasterData(
+        merchants,
+        categories,
+        tags,
+      ),
+      createTransaction = CreateTransaction(
+        transactions,
+        const SystemApplicationClock(),
+      ),
+      createReceiptTransaction = CreateReceiptTransaction(
+        transactions,
+        const SystemApplicationClock(),
+      ),
+      importTransaction = ImportTransaction(
+        transactions,
+        const SystemApplicationClock(),
+      ),
+      getTransaction = GetTransaction(transactions),
+      updateTransaction = UpdateTransaction(
+        transactions,
+        const SystemApplicationClock(),
+      ),
+      archiveTransaction = ArchiveTransaction(
+        transactions,
+        const SystemApplicationClock(),
+      ),
+      restoreTransaction = RestoreTransaction(
+        transactions,
+        const SystemApplicationClock(),
+      ),
+      deleteTransactionPermanently = DeleteTransactionPermanently(transactions),
+      listPaymentSources = ListPaymentSources(paymentSources),
+      savePaymentSource = SavePaymentSource(paymentSources),
+      archivePaymentSource = ArchivePaymentSource(paymentSources),
+      assignPaymentSource = AssignPaymentSource(
+        transactions,
+        paymentSources,
+        const SystemApplicationClock(),
+      ),
+      listMerchants = ListMerchants(merchants),
+      listCategories = ListCategories(categories),
+      listTags = ListTags(tags),
+      saveMerchant = SaveMerchant(merchants),
+      saveCategory = SaveCategory(categories),
+      saveTag = SaveTag(tags),
+      assignMerchant = AssignMerchant(
+        transactions,
+        merchants,
+        const SystemApplicationClock(),
+      ),
+      assignCategory = AssignCategory(
+        transactions,
+        categories,
+        const SystemApplicationClock(),
+      ),
+      addTag = AddTag(transactions, tags, const SystemApplicationClock()),
+      removeTag = RemoveTag(transactions, const SystemApplicationClock()),
+      listReviewItems = ListReviewItems(transactions),
+      resolveReviewIssue = ResolveReviewIssue(
+        transactions,
+        const SystemApplicationClock(),
+      ),
+      dismissReviewIssue = DismissReviewIssue(
+        transactions,
+        const SystemApplicationClock(),
+      ),
+      listEvidenceForTransaction = ListEvidenceForTransaction(evidence),
+      loadUserPreference = LoadUserPreference(preferences),
+      saveUserPreference = SaveUserPreference(preferences),
+      storeAndAttachEvidence = StoreAndAttachEvidence(evidence),
+      removeEvidence = RemoveEvidence(evidence),
+      saveExtraction = SaveExtraction(evidence);
+
   final ListTransactions listTransactions;
   final SeedInitialMasterData seedInitialMasterData;
   final CreateTransaction createTransaction;
