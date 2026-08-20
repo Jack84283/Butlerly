6e8e65782f6747315bb8df78d8ef9b1f21058d49
Jack <jack84283@gmail.com>
2026-08-20 15:20:07 -0700

    Add receipt extraction lookup use case

---
+++ packages/butlerly_finance_application/lib/src/use_cases/evidence_use_cases.dart
@@ -0,0 +1,14 @@
+import 'package:butlerly_finance_domain/butlerly_finance_domain.dart';
+
+import '../result/application_result.dart';
+
+final class GetExtractionForEvidence {
+  const GetExtractionForEvidence(this.repository);
+
+  final EvidenceRepository repository;
+
+  Future<ApplicationResult<Extraction?>> call(String evidenceId) =>
+      runApplication('get evidence extraction', () {
+        return repository.findExtractionForEvidence(EvidenceId(evidenceId));
+      });
+}
