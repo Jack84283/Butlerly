4c1d247672d5cb25f4b9004f238cf790c62d4f30
Jack <jack84283@gmail.com>
2026-08-20 15:19:27 -0700

    Add extraction lookup to evidence repository

--- packages/butlerly_finance_domain/lib/src/repositories/finance_repositories.dart
+++ packages/butlerly_finance_domain/lib/src/repositories/finance_repositories.dart
@@ -69,6 +69,7 @@ abstract interface class TagRepository {
 abstract interface class EvidenceRepository {
   Future<void> save(EvidenceItem evidence);
   Future<void> saveExtraction(Extraction extraction);
+  Future<Extraction?> findExtractionForEvidence(EvidenceId id);
   Future<void> link(AttachmentLink link);
   Future<EvidenceItem?> findById(EvidenceId id);
   Future<List<EvidenceItem>> listForTransaction(TransactionId id);
