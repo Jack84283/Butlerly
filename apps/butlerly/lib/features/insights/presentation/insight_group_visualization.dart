350287ad432901a769c56bfd7ebfb81580d97314
Jack <jack84283@gmail.com>
2026-09-11 14:49:58 -0700

    fix(insights): remove unused application import

--- apps/butlerly/lib/features/insights/presentation/insight_group_visualization.dart
+++ apps/butlerly/lib/features/insights/presentation/insight_group_visualization.dart
@@ -5,7 +5,6 @@ import 'package:butlerly/features/insights/presentation/insight_presentation.dar
 import 'package:butlerly/features/insights/presentation/insight_visualization.dart';
 import 'package:butlerly/l10n/app_localizations.dart';
 import 'package:butlerly/l10n/finance_formatters.dart';
-import 'package:butlerly_finance_application/butlerly_finance_application.dart';
 import 'package:butlerly_finance_domain/butlerly_finance_domain.dart';
 import 'package:flutter/material.dart';
 
