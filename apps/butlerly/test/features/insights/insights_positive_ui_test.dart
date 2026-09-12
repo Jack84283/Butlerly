3a7c1e70ba9c58b7ade9e064a7745cb069625ecc
Jack <jack84283@gmail.com>
2026-09-12 13:05:42 -0700

    Align grouped bar test with authored rule copy

--- apps/butlerly/test/features/insights/insights_positive_ui_test.dart
+++ apps/butlerly/test/features/insights/insights_positive_ui_test.dart
@@ -220,7 +220,7 @@ void main() {
     );
     await tester.pumpAndSettle();
 
-    expect(find.text('Categories'), findsOneWidget);
+    expect(find.text('Analysis summary'), findsOneWidget);
     expect(find.byType(InsightBarVisualization), findsOneWidget);
   });
 
