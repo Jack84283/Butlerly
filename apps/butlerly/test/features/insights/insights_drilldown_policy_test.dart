709e053575ae33ecf4460798f31b810ee38e2976
Jack <jack84283@gmail.com>
2026-09-12 12:09:33 -0700

    test(insights): follow combined supporting transaction CTA

--- apps/butlerly/test/features/insights/insights_drilldown_policy_test.dart
+++ apps/butlerly/test/features/insights/insights_drilldown_policy_test.dart
@@ -55,7 +55,7 @@ void main() {
     await tester.drag(find.byType(CustomScrollView), const Offset(0, -500));
     await tester.pumpAndSettle();
 
-    await tester.tap(find.text('View transactions'));
+    await tester.tap(find.text('View 1 supporting transaction'));
     expect(
       path,
       '/search?locked=true&from=2026-09-01&to=2026-09-05&ids=support-1',
@@ -87,7 +87,7 @@ void main() {
     await tester.drag(find.byType(CustomScrollView), const Offset(0, -500));
     await tester.pumpAndSettle();
 
-    await tester.tap(find.text('View transactions'));
+    await tester.tap(find.text('View 1 supporting transaction'));
     expect(
       path,
       '/search?locked=true&from=2026-09-01&to=2026-09-05&direction=expense&category=category.dining',
@@ -113,7 +113,7 @@ void main() {
     await tester.drag(find.byType(CustomScrollView), const Offset(0, -500));
     await tester.pumpAndSettle();
 
-    await tester.tap(find.text('View transactions'));
+    await tester.tap(find.text('View 1 supporting transaction'));
     expect(
       path,
       '/search?locked=true&from=2026-09-01&to=2026-09-05&direction=expense',
@@ -139,7 +139,7 @@ void main() {
     await tester.drag(find.byType(CustomScrollView), const Offset(0, -500));
     await tester.pumpAndSettle();
 
-    await tester.tap(find.text('View transactions'));
+    await tester.tap(find.text('View 1 supporting transaction'));
     expect(
       path,
       '/search?locked=true&from=2026-09-01&to=2026-09-05&direction=expense&ids=large-expense&insightRule=ANL-R024',
@@ -175,7 +175,7 @@ void main() {
     await tester.drag(find.byType(CustomScrollView), const Offset(0, -500));
     await tester.pumpAndSettle();
 
-    await tester.tap(find.text('View transactions'));
+    await tester.tap(find.text('View 1 supporting transaction'));
     expect(
       path,
       '/search?locked=true&from=2026-09-01&to=2026-09-05&ids=support-1',
