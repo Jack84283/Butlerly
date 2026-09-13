10c00b2aa20ba84c7ec981d9c7ccd055b51bc3d6
Jack <jack84283@gmail.com>
2026-09-12 18:19:17 -0700

    Bump database schema for merge metadata

--- packages/butlerly_database/lib/src/database/butlerly_database.dart
+++ packages/butlerly_database/lib/src/database/butlerly_database.dart
@@ -12,7 +12,7 @@ final class ButlerlyDatabase {
     this.migrations = const {},
   });
 
-  static const databaseVersion = 7;
+  static const databaseVersion = 8;
 
   final DatabaseFactory factory;
   final String path;
