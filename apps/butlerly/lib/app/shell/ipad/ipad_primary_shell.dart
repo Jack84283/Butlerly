6bf37798f8fa9d934ac959c0e580b2565d01dab7
Jack <jack84283@gmail.com>
2026-09-16 17:52:17 -0700

    Let iPad navigation arch overlap body

--- apps/butlerly/lib/app/shell/ipad/ipad_primary_shell.dart
+++ apps/butlerly/lib/app/shell/ipad/ipad_primary_shell.dart
@@ -20,6 +20,7 @@ class IPadPrimaryShell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Scaffold(
+    extendBody: true,
     body: SafeArea(
       bottom: false,
       child: ColoredBox(
