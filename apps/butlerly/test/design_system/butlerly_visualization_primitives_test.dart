260cdf04ed8ed02a14cd47b0bdbf8903a83c289c
Jack <jack84283@gmail.com>
2026-09-17 18:12:40 -0700

    Format shared visualization tests

--- apps/butlerly/test/design_system/butlerly_visualization_primitives_test.dart
+++ apps/butlerly/test/design_system/butlerly_visualization_primitives_test.dart
@@ -70,6 +70,9 @@ void main() {
     );
     await tester.pumpAndSettle();
 
-    expect(find.bySemanticsLabel('Jan: 10.0 USD, Feb: 20.0 USD'), findsOneWidget);
+    expect(
+      find.bySemanticsLabel('Jan: 10.0 USD, Feb: 20.0 USD'),
+      findsOneWidget,
+    );
   });
 }
