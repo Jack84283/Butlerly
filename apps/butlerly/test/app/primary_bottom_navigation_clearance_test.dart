c25f6035115250f0e06c040c3a82ddcac15fe313
Jack <jack84283@gmail.com>
2026-09-20 08:53:39 -0700

    Match Dart formatting in footer navigation test

--- apps/butlerly/test/app/primary_bottom_navigation_clearance_test.dart
+++ apps/butlerly/test/app/primary_bottom_navigation_clearance_test.dart
@@ -89,7 +89,9 @@ void main() {
 
     final homeLabelTop = tester.getTopLeft(find.text('Home')).dy;
     final addLabelTop = tester.getTopLeft(find.text('Add')).dy;
-    final transactionsLabelTop = tester.getTopLeft(find.text('Transactions')).dy;
+    final transactionsLabelTop = tester.getTopLeft(
+      find.text('Transactions'),
+    ).dy;
     final moreLabelTop = tester.getTopLeft(find.text('More')).dy;
     expect(homeLabelTop, closeTo(addLabelTop, 0.01));
     expect(transactionsLabelTop, closeTo(addLabelTop, 0.01));
