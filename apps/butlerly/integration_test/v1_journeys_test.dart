d5378a4b2aeb62cc246075ccd49d13598efe0e2f
Jack <jack84283@gmail.com>
2026-09-20 09:11:10 -0700

    Fix integration test text input hide calls

--- apps/butlerly/integration_test/v1_journeys_test.dart
+++ apps/butlerly/integration_test/v1_journeys_test.dart
@@ -92,12 +92,12 @@ void main() {
     );
     expect(amountField, findsOneWidget);
     await tester.enterText(amountField, '18.75');
-    await tester.testTextInput.hide();
+    tester.testTextInput.hide();
     await tester.fling(find.byType(ListView).last, const Offset(0, -500), 1000);
     await tester.pump(const Duration(milliseconds: 500));
     expect(descriptionField, findsOneWidget);
     await tester.enterText(descriptionField, 'UI merchant');
-    await tester.testTextInput.hide();
+    tester.testTextInput.hide();
     await tester.fling(find.byType(ListView).last, const Offset(0, -500), 1000);
     await tester.pump(const Duration(milliseconds: 500));
     expect(find.text('Save locally'), findsOneWidget);
