a56c79875795dc2b4a989093556b026963436c01
Jack <jack84283@gmail.com>
2026-09-20 08:33:29 -0700

    Apply Dart formatting to footer navigation

--- apps/butlerly/lib/app/shell/shared/primary_bottom_navigation.dart
+++ apps/butlerly/lib/app/shell/shared/primary_bottom_navigation.dart
@@ -117,9 +117,7 @@ class PrimaryBottomNavigation extends StatelessWidget {
       children: [
         icon,
         SizedBox(
-          height: add
-              ? ButlerlySpacing.none
-              : ButlerlySize.navigationLabelGap,
+          height: add ? ButlerlySpacing.none : ButlerlySize.navigationLabelGap,
         ),
         Text(
           destination.label,
