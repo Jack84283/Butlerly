edd30237055379cae42013e74dee2959e9748aa7
Jack <jack84283@gmail.com>
2026-09-23 19:40:25 -0700

    Add settlement tombstones to baseline schema

--- packages/butlerly_database/database/schema/v1.sql
+++ packages/butlerly_database/database/schema/v1.sql
@@ -602,3 +602,17 @@ CREATE INDEX payment_settlements_period_idx
 CREATE INDEX transactions_payment_source_date_idx
   ON transactions(payment_source_id, transaction_date);
 
+CREATE TRIGGER payment_settlements_tombstone AFTER DELETE ON payment_settlements
+BEGIN
+  INSERT INTO entity_tombstones(entity_type, entity_id, deleted_at)
+  VALUES('payment_settlements', OLD.id, strftime('%Y-%m-%dT%H:%M:%fZ','now'))
+  ON CONFLICT(entity_type, entity_id)
+  DO UPDATE SET deleted_at = excluded.deleted_at;
+END;
+
+CREATE TRIGGER payment_settlements_tombstone_clear AFTER INSERT ON payment_settlements
+BEGIN
+  DELETE FROM entity_tombstones
+  WHERE entity_type = 'payment_settlements' AND entity_id = NEW.id;
+END;
+
