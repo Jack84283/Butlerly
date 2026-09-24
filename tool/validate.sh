20c6e00a7657413bbcde62c6dd9f5de1ea3624af
Jack <jack84283@gmail.com>
2026-09-23 19:47:51 -0700

    Remove temporary backup formatter diagnostic

--- tool/validate.sh
+++ tool/validate.sh
@@ -35,7 +35,6 @@ validate_database() (
 
 validate_flutter_application() (
   cd apps/butlerly
-  dart format --output=show test/core/data/local_backup_manager_test.dart
   format_check
   flutter analyze
   flutter test
