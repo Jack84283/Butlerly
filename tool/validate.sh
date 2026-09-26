68fada3ca9a2609983767372efdfff6bc2822417
Jack <jack84283@gmail.com>
2026-09-25 18:48:52 -0700

    ci: show settlement formatter diff

--- tool/validate.sh
+++ tool/validate.sh
@@ -35,10 +35,9 @@ validate_database() (
 
 validate_flutter_application() (
   cd apps/butlerly
-  format_check
-  flutter analyze
-  flutter test
-  flutter build web
+  dart format .
+  git diff -- lib/features/tools/presentation/payment_settlements_page.dart
+  exit 1
 )
 
 validate_integration_test() (
